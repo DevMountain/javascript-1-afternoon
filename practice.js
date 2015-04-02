530e0eeece7ea5a3c7fd0de5a5a0e75ee641bcf5
guybrud <guybrud@gmail.com>
2015-04-02 11:02:29 -0600

    just finished, but may change

--- practice.js
+++ practice.js
@@ -6,7 +6,14 @@ var name = 'Tyler';
 //If the argument you passed in is equal to 'Tyler', return true. If it's not, return false.
 
   //Code Here
-
+var isTyler = function(name) {
+	if (name === "Tyler") {
+		return true;
+	}
+	else {
+		return false;
+	}
+}
 
 //Next problem
 
@@ -16,6 +23,10 @@ var name = 'Tyler';
 
 
   //Code Here
+  var getName = function() {
+  	 var name = prompt("what is your name?");
+  	return name;
+  }
 
 
 //Next Problem
@@ -26,7 +37,11 @@ var name = 'Tyler';
 //then alerts "Welcome, " plus whatever the users name is.
 
   //Code Here
-
+var welcome = function() {
+	getName() {
+		alert("Welcome" + name);
+	}
+}
 
 //Next problem
 
@@ -36,14 +51,16 @@ var name = 'Tyler';
 //What is the difference between arguments and parameters?
 
   //Answer Here
-
+parameters are the placeholder of the argument
+the argument is the information you want to pass throught the function.
 
 //Next problem
 
 
 
 //What are all the falsy values in JavaScript and how do you check if something is falsy?
-
+false 0 NaN undefined null ""
+use an if statement to check against truthyness, and if it isnt trthy it is falsy
 
   //Answer Here
 
@@ -56,25 +73,35 @@ var name = 'Tyler';
 //Create a function called myName that returns your name
 
   //Code Here
-
+function nameMe() {
+	return "Ryan Brudnicki";
+}
 //Now save the function definition of myName into a new variable called newMyName
 
   //Code Here
-
+var newMyName = nameMe();
 //Now alert the result of invoking newMyName
 
-
+alert(newMyName);
 
 //Next problem
 
 
 
 //Create a function called outerFn which returns an anonymous function which returns your name.
+function outerFn() {
+	return function() {
+		return "ryan brudnicki";
+	};
+};
+
 
   //Code Here
+
 
 //Now save the result of invoking outerFn into a variable called innerFn.
 
   //Code Here
-
+var innerFn = outerFn()();
 //Now invoke innerFn.
+console.log(innerFn);
