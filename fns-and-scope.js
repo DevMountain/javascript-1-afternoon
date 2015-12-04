b4e8d38eb02eaa3afc37d4830b1bd9c12522fc48
Joe Neuman <Joe Neuman>
2015-12-04 15:33:56 -0700

    asdf

---
+++ fns-and-scope.js
@@ -0,0 +1,81 @@
+//Once you complete a problem, open up Chrome and check the answer in the console.
+
+
+var name = 'Tyler';
+//Create a function called isTyler that accepts name as it's only parameter.
+//If the argument you passed in is equal to 'Tyler', return true. If it's not, return false.
+
+  //Code Here
+
+//Next problem
+
+
+
+//Create a function called getName that uses prompt() to prompt the user for their name, then returns the name.
+
+
+  //Code Here
+
+
+//Next Problem
+
+
+
+//Create a function called welcome that uses your getName function you created in the previous problem to get the users name,
+//then alerts "Welcome, " plus whatever the users name is.
+
+  //Code Here
+
+
+//Next problem
+
+
+
+
+//What is the difference between arguments and parameters?
+
+  //Answer Here
+
+
+//Next problem
+
+
+
+//What are all the falsy values in JavaScript and how do you check if something is falsy?
+
+
+  //Answer Here
+
+
+
+//Next Problem
+
+
+
+//Create a function called myName that returns your name
+
+  //Code Here
+  
+
+
+//Now save the function definition of myName into a new variable called newMyName
+
+  //Code Here
+
+//Now alert the result of invoking newMyName
+
+
+
+//Next problem
+
+
+
+//Create a function called outerFn which returns an anonymous function which returns your name.
+
+  //Code Here
+
+//Now save the result of invoking outerFn into a variable called innerFn.
+
+  //Code Here
+
+//Now invoke innerFn.
