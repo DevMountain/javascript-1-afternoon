9698bd468336f17dbabb83c502bf1b0df740b99d
ascotson <ascotson@gmail.com>
2016-02-08 13:52:15 -0700

    Restoring files.

---
+++ README.md
@@ -0,0 +1,25 @@
+<img src="https://devmounta.in/img/logowhiteblue.png" width="250" align="right">
+
+JS-Basics
+=========
+
+##Objective
+Better understand Javascript fundamentals by completing a series of activities
+
+##Directions
+Fork this repo, clone your fork, then open fns-scopes.js and complete all the activities then open closures.js and complete all of those activities.
+*COMMIT AND PUSH YOUR CODE OFTEN!*
+
+### Debugging/Visualization tools : 
+One of the biggest tools in a developers disposal is the debugger. Chrome has one built-in. To use it, just open the index.html file in your browser.
+
+Another great tool for small and isolated pieces of code is : http://www.pythontutor.com/visualize.html#
+
+## Contributions
+If you see a problem or a typo, please fork, make the necessary changes, and create a pull request so we can review your changes and merge them into the master repo and branch.
+
+## Copyright
+
+© DevMountain LLC, 2015. Unauthorized use and/or duplication of this material without express and written permission from DevMountain, LLC is strictly prohibited. Excerpts and links may be used, provided that full and clear credit is given to DevMountain with appropriate and specific direction to the original content.
+
+<img src="https://devmounta.in/img/logowhiteblue.png" width="250">
---
+++ closures.js
@@ -0,0 +1,127 @@
+//Once you complete a problem, open up Chrome and check the answer in the console.
+
+var outer = function(){
+  var name = 'Tyler';
+  return function(){
+    return 'The original name was ' + name;
+  }
+}
+//Above you're given a function that returns another function which has a closure over the name variable.
+//Invoke outer saving the return value into another variable called 'inner'.
+
+  //Code Here
+
+//Once you do that, invoke inner.
+
+  //Code Here
+
+
+
+//Next problem
+
+
+
+var callFriend = function(){
+  var friend = 'Jake';
+  function callF(number){
+    return 'Calling ' + friend + ' at ' + number;
+  }
+  return callF;
+};
+
+//Above you're given a callFriend function that returns another function.
+//Do what you need to do in order to call your function and get 'Calling Jake at 435-215-9248' in your console.
+
+  //Code Here
+
+
+
+//Next Problem
+
+
+
+/*
+  Write a function called makeCounter that makes the following code work properly.
+*/
+
+  //Code Here
+  var count = makeCounter();
+  count(); // 1
+  count(); // 2
+  count(); // 3
+  count(); // 4
+
+
+
+//Next Problem
+
+
+
+/*
+  Write a function named codeLove that returns the string 'I love code'. Write a second function named codeFriend that accepts the first function as it's first parameter. The second function should return a new third function. Store the third function in a variable, codeEcho which, when invoked, invokes the first, original function that was passed in, but will only ever do so once (returns null after first invocation).
+*/
+
+  //Code Here
+
+
+
+//Next Problem
+
+
+
+/*
+  Now, similar to the last problem, write a function called 'fnCounter' that accepts two parameters. The first parameter will be an anonymous function and the second parameter, 'N', will be a number. Now, in 'fnCounter', allow the anonymous funciton to be invoked 'N' number of times. After it's been invoked 'N' number of times, return 'STOP'.
+*/
+
+
+
+//Next Problem
+
+
+
+/*
+  var counter = function(){
+    for (var i=1; i<=5; i++) {
+      setTimeout( function timer(){
+          console.log( i );
+      }, i*1000 );
+    }
+  };
+
+  Above you have a function named counter. Examine the function (without running the code) then below write what you expect to happen when the funciton is invoked. *Hint: setTimeout calls a function or evaluates an expression after a specified number of milliseconds.
+
+    //Answer Here
+
+
+  Now, run the function in your console and note what happpens.
+
+  Was your answer right or wrong?
+
+    //Answer Here
+
+
+  Fix the counter function so that it works the way you expect it to work. (logging 1 then 2 then 3, etc) (Note: No unit test for this one because of the timeout)
+*/
+
+    //Code Here
+
+
+
+//Next Problem
+
+
+
+/*
+  Make the following code work
+
+  funcArray[0]() //0
+  funcArray[1]() //1
+  funcArray[2]() //2
+  funcArray[3]() //3
+  funcArray[4]() //4
+  funcArray[5]() //5
+
+  *Hint: Don't let this fool you. Break down what's really happening here.
+*/
+
+
