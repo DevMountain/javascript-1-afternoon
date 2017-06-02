f0145af070fe8ddf6dffe17c3ea40fe3caa81a25
Joe Isabell <joeisabell@me.com>
2017-06-02 12:13:53 -0600

    finished js-basics

--- fns-and-scope.js
+++ fns-and-scope.js
@@ -34,8 +34,6 @@ var name = 'Tyler';
 //////////////////PROBLEM 4////////////////////
 
 
-
-
 //What is the difference between arguments and parameters?
 
   //When declaring functions you give them parameters, when invoking functions
@@ -49,7 +47,7 @@ var name = 'Tyler';
 //What are all the falsy values in JavaScript and how do you check if something is falsy?
 
 
-  //
+  //Null, undefined, false, 0, '', NaN -- You can check if something is falsy with val == false
 
 
 
@@ -59,17 +57,19 @@ var name = 'Tyler';
 
 //Create a function called myName that returns your name
 
-  //Code Here
+  function myName(){
+    return 'Joseph Isabell';
+  }
 
 
 
 //Now save the function definition of myName into a new variable called newMyName
 
-  //Code Here
+  var newMyName = myName;
 
 //Now alert the result of invoking newMyName
 
-
+  alert('Now invoking newMyName: ' + newMyName())
 
 //////////////////PROBLEM 7////////////////////
 
@@ -77,10 +77,16 @@ var name = 'Tyler';
 
 //Create a function called outerFn which returns an anonymous function which returns your name.
 
-  //Code Here
+  function outerFn(){
+    return function(){
+      return 'Joseph Isabell';
+    }
+  }
 
 //Now save the result of invoking outerFn into a variable called innerFn.
 
-  //Code Here
+  var innerFn = outerFn();
 
 //Now invoke innerFn.
+
+  console.log(innerFn());
