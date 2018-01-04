bbf9f135ae0083dc494ec2014329e1dd77094009
jameslemire <webdev.jameslemire@gmail.com>
2018-01-04 12:35:31 -0700

    JL master - Create a similar feel across the JS projects.

--- practice.js
+++ practice.js
@@ -1,79 +1,115 @@
-//////////////////PROBLEM 1////////////////////
+/* 
+  Once you complete a problem, refresh ./SpecRunner.html in your browser and check to see if the problem's test(s) are passing.
+  Passed tests will be indicated by a green circle.
+  Failed tests will be indicated by a red X.
 
+  You can refresh the page at any time to re-run all the tests.
+*/
+
+////////// PROBLEM 1 //////////
+
+// Do not edit the code below.
 var name = 'Tyler';
-//Create a function called isTyler that accepts name as it's only parameter.
-//If the argument you passed in is equal to 'Tyler', return true. If it's not, return false.
+// Do not edit the code above.
 
-  //Code Here
+/*
+  Create a function called isTyler that accepts name as it's only parameter.
+  If the argument you passed in is equal to 'Tyler', return true. If it's not, return false.
+*/
 
-//////////////////PROBLEM 2////////////////////
-
-
-//Create a function called getName that uses prompt() to prompt the user for their name, then returns the name.
-
-
-  //Code Here
-
-
-//////////////////PROBLEM 3////////////////////
+//Code Here
 
 
 
-//Create a function called welcome that uses your getName function you created in the previous problem to get the users name,
-//then alerts "Welcome, " plus whatever the users name is.
+////////// PROBLEM 2 //////////
 
-  //Code Here
+/*
+  Create a function called getName that uses prompt() to prompt the user for their name and then returns the given name.
+*/
 
-
-//////////////////PROBLEM 4////////////////////
-
-
-
-
-//What is the difference between arguments and parameters?
-
-  //Answer Here
-
-
-//////////////////PROBLEM 5////////////////////
+//Code Here
 
 
 
-//What are all the falsy values in JavaScript and how do you check if something is falsy?
+////////// PROBLEM 3 //////////
+
+/*
+  Create a function called welcome that uses your getName function you created in the previous problem to get the user's name.
+  Then alert "Welcome, " plus the given user's name.
+
+  Example: "Welcome, Bob Joe"
+*/
+
+//Code Here
 
 
-  //Answer Here
+
+////////// PROBLEM 4 //////////
+
+/*
+  What is the difference between arguments and parameters?
+*/
+
+//Answer Here
 
 
 
-//////////////////PROBLEM 6////////////////////
+////////// PROBLEM 5 //////////
+
+/*
+  What are all the falsy values in JavaScript and how do you check if something is falsy?
+*/
+
+//Answer Here
 
 
 
-//Create a function called myName that returns your name
+////////// PROBLEM 6 //////////
 
-  //Code Here
+/*
+  Create a function called myName that returns your name
+*/
+
+//Code Here
   
 
 
-//Now save the function definition of myName into a new variable called newMyName
+/*
+  Now save the function definition of myName into a new variable called newMyName
+*/
 
-  //Code Here
-
-//Now alert the result of invoking newMyName
-
-
-
-//////////////////PROBLEM 7////////////////////
+//Code Here
 
 
 
-//Create a function called outerFn which returns an anonymous function which returns your name.
+/*
+  Now alert the result of invoking newMyName
+*/
 
-  //Code Here
+// Code Here
 
-//Now save the result of invoking outerFn into a variable called innerFn.
 
-  //Code Here
 
-//Now invoke innerFn.
+////////// PROBLEM 7 //////////
+
+/*
+  Create a function called outerFn which returns an anonymous function which returns your name.
+*/
+
+//Code Here
+
+
+
+/*
+  Now save the result of invoking outerFn into a variable called innerFn.
+*/
+
+//Code Here
+
+
+
+/* 
+  Now invoke innerFn.
+*/
+
+// Code Here
