36f2fd30fecb1c15a6ee591f8d2e669eae51ffb0
Joe Toh Djojo <joseph.tohdjojo@gmail.com>
2018-10-01 23:45:17 -0400

    Reformat and set character line limit to 80

--- practice.js
+++ practice.js
@@ -1,99 +1,118 @@
-//////////////////PROBLEM 1////////////////////
+/**
+ * PROBLEM 1
+ *
+ * Create a function declaration called greeting that accepts name as its only
+ * parameter. Greeting should return the string "Hello, " plus the value of the
+ * name parameter.
+ */
 
-//Create a function declaration called greeting that
-//accepts name as its only parameter.
-//greeting should return the string "Hello, "
-//plus the value of the name parameter.
+// Code here
 
-//Code here
+/**
+ * PROBLEM 2
+ *
+ * Rewrite the function greeting as a function expression. Name it newGreeting.
+ */
 
-//////////////////PROBLEM 2////////////////////
+// Code Here
 
-//Rewrite the function greeting as a function expression.
-//Name it newGreeting.
+/**
+ * PROBLEM 3
+ *
+ * Rewrite the function greeting as an arrow function. Name it finalGreeting.
+ */
+
+// Code Here
+
+/**
+ * PROBLEM 4
+ *
+ * Create an array called groceries with the values "apples", "milk", "eggs",
+ * "bread"
+ */
+
+// Code Here
+
+/**
+ * Write a function called doubleCheck that takes in an array as a parameter. If
+ * the array does not contain "chocolate", add "chocolate". doubleCheck should
+ * return the array.
+ */
+
+// Code Here
+
+/**
+ * PROBLEM 5
+ *
+ * Create an object saved to the variable dog. The dog object should have the
+ * following properties: name (a string), color: (a string), age (a number), and
+ * goodBoy (a boolean).
+ */
+
+// Code Here
+
+/**
+ * ...access the dog's name from the object and assign it to a variable called
+ * devMountainClassPet.
+ */
+
+// Code Here
+
+/**
+ * Add a method to dog called bark. The value of bark should be a function that
+ * returns the string "Woof woof".
+ */
+
+// Code Here
+
+/**
+ * Store the result of invoking the bark method in a variable called ruff.
+ */
+
+// Code Here
+
+/**
+ * PROBLEM 6
+ *
+ * a function called looper that takes in an array. looper should declare a
+ * variable called mySum and set it equal to 0. looper should then loop through
+ * the array and check each element.
+ *
+ * If the element is odd, or if the element is greater than or equal to 100, add
+ * the elementto the mySum variable total.
+ *
+ * Return mySum.
+ */
+
 
 //Code Here
 
-//////////////////PROBLEM 3////////////////////
-
-//Rewrite the function greeting as an arrow function.
-//Name it finalGreeting.
-
-//Code Here
-
-//////////////////PROBLEM 4////////////////////
-
-//Create an array called groceries with the values
-//"apples", "milk", "eggs", "bread"
-
-//Code Here
-
-//Write a function called doubleCheck that takes in an array
-//as a parameter.
-
-//If the array does not contain "chocolate", add "chocolate".
-//doubleCheck should return the array.
-
-//Code Here
-
-//////////////////PROBLEM 5////////////////////
-
-//Create an object saved to the variable dog.
-//The dog object should have the following properties:
-//name (a string), color: (a string), age (a number),
-//and goodBoy (a boolean).
-
-//Code Here
-
-//...access the dog's name from the object and assign it to a
-//variable called devMountainClassPet.
-
-//Code Here
-
-//Add a method to dog called bark.
-//The value of bark should be a function that returns the string "Woof woof".
-
-//Code Here
-
-//Store the result of invoking the bark method in a variable called ruff.
-
-//Code Here
-
-//////////////////PROBLEM 6////////////////////
-
-//Write a function called looper that takes in an array. looper should declare
-//a variable called mySum and set it equal to 0. looper should then loop through
-//the array and check each element.
-
-//If the element is odd, or if the element is greater than or equal to 100, add the element
-//to the mySum variable total.
-
-//Return mySum.
-
-//Code Here
-
-//////////////////PROBLEM 7////////////////////
-
-//Given the following function called math
+/**
+ * PROBLEM 7
+ *
+ * Given the following function math write a function called add that takes in
+ * two parameters and returns the result of adding them together.
+ */
 
 function math(num1, num2, callback) {
   return callback(num1, num2);
 }
 
-//Write a function called add that takes in two parameters and
-//returns the result of adding them together.
+//Code Here
+
+/**
+ * Now invoke math, passing in the numbers 3 and 4, and your add function,
+ * storing the result in the variable mathSum.
+ */
 
 //Code Here
 
-//Now invoke math, passing in the numbers 3 and 4, and your add function,
-//storing the result in the variable mathSum.
-
-//Code Here
-
-//////////////////PROBLEM 8////////////////////
-
-//Write a function called invoker that takes in one paramter, a callback function.
-//invoker should return the result of invoking the callback.
+/**
+ * PROBLEM 8
+ *
+ * Write a function called invoker that takes in one paramter, a callback
+ * function. invoker should return the result of invoking the callback.
+ */
 
 function sampleCallbackOne() {
   return "I am a callback function";
@@ -105,12 +124,19 @@ function sampleCallbackTwo() {
 
 //Code Here
 
-//////////////////PROBLEM 9////////////////////
+/**
+ * PROBLEM 9
+ *
+ * There are 4 variables: duck, rubberDuck, sailorDuck and realDuck all
+ * within different scopes. Given the functions and variables, edit the arrays
+ * below to contain only the appropriate variable names as strings.
+*/
 
 let duck = "cute";
 
 function bathroom() {
   let rubberDuck = "squeaky";
+
   function bathtub() {
     let sailorDuck = "nautical";
   }
@@ -120,35 +146,39 @@ function pond() {
   let realDuck = "fluffy";
 }
 
-//There are 4 variables above: duck, rubberDuck, sailorDuck and realDuck
-//all within different scopes.
-//Given the functions and variables above, edit the arrays
-//below to contain only the appropriate variable names
-//as strings.
-
-//This array should contain the variable names (as strings) accessible in the global scope.
+// This array should contain the variable names (as strings) accessible in the
+// global scope.
 let globalScope = ["duck", "sailorDuck", "rubberDuck", "realDuck"];
 
-//This array should contain the variable names (as strings) accessible in the bathroom function.
+// This array should contain the variable names (as strings) accessible in the
+// bathroom function.
 let bathroomScope = ["duck", "sailorDuck", "rubberDuck", "realDuck"];
 
-//This array should contain the variable names (as strings) accessible in the bathtub function.
+// This array should contain the variable names (as strings) accessible in the
+// bathtub function.
 let bathtubScope = ["duck", "sailorDuck", "rubberDuck", "realDuck"];
 
-//This array should contain the variable names (as strings) accessible in the pond function.
+// This array should contain the variable names (as strings) accessible in the
+// pond function.
 let pondScope = ["duck", "sailorDuck", "rubberDuck", "realDuck"];
 
-//////////////////PROBLEM 10////////////////////
-
-//Create a function called outerFn which returns an anonymous
-//function which returns your name.
-
-//Code Here
-
-//Now save the result of invoking outerFn into a variable called innerFn.
+/**
+ * PROBLEM 10
+ *
+ * Create a function called outerFn which returns an anonymous function which
+ * returns your name.
+ */
 
 //Code Here
 
-//Now invoke innerFn and save the result to a variable called finalResult.
+/**
+ * Now save the result of invoking outerFn into a variable called innerFn.
+ */
+
+//Code Here
+
+/**
+ * Now invoke innerFn and save the result to a variable called finalResult.
+ */
 
 //Code Here
--- test/spec/practiceSpec.js
+++ test/spec/practiceSpec.js
@@ -36,14 +36,17 @@ describe("js-day1-basic-assessment", function() {
     it("should exist", function() {
       expect(newGreeting).toBeDefined();
     });
+
     it("should be made with an anonymous functon assignment", function() {
       let str = newGreeting.toString();
       let match = !!str.match(/function\s*\(/);
       expect(match).toBe(true);
     });
+
     it('should return "Hello, Trogdor" if passed "Trogdor"', function() {
       expect(newGreeting("Trogdor")).toBe("Hello, Trogdor");
     });
+
     it('should return "Hello, undefined" if passed nothing', function() {
       expect(newGreeting()).toBe("Hello, undefined");
     });
@@ -53,14 +56,17 @@ describe("js-day1-basic-assessment", function() {
     it("should exist", function() {
       expect(finalGreeting).toBeDefined();
     });
+
     it("should be made with an arrow function assignment", function() {
       let str = finalGreeting.toString();
       let match = !!str.match(/=>/);
       expect(match).toBe(true);
     });
+
     it('should return "Hello, Trogdor" if passed "Trogdor"', function() {
       expect(finalGreeting("Trogdor")).toBe("Hello, Trogdor");
     });
+
     it('should return "Hello, undefined" if passed nothing', function() {
       expect(finalGreeting()).toBe("Hello, undefined");
     });
@@ -70,6 +76,7 @@ describe("js-day1-basic-assessment", function() {
     it("groceries should exist", function() {
       expect(groceries).toBeDefined();
     });
+
     it("groceries should contain given values", function() {
       let vals = ["apples", "milk", "eggs", "bread"];
       let allGood = arrayIncludes(groceries, vals);
@@ -80,11 +87,13 @@ describe("js-day1-basic-assessment", function() {
       let vals = ["apples", "milk", "eggs", "bread"];
       expect(Array.isArray(doubleCheck(vals))).toBe(true);
     });
+
     it("doubleCheck should return array that contains chocolate only once", function() {
       let vals = ["apples", "milk", "eggs", "bread"];
       let newArr = doubleCheck(vals).filter(val => val === "chocolate");
       expect(newArr.length).toBe(1);
     });
+
     it("doubleCheck should not remove anything from the array", function() {
       let vals = ["apples", "milk", "eggs", "bread"];
       let newArr = doubleCheck(vals);
@@ -100,12 +109,15 @@ describe("js-day1-basic-assessment", function() {
       expect(typeof dog.age).toEqual("number");
       expect(typeof dog.goodBoy).toEqual("boolean");
     });
+
     it("should properly assign the dog name", function() {
       expect(devMountainClassPet).toBe(dog.name);
     });
+
     it("dog should have a bark method", function() {
       expect(typeof dog.bark).toBe("function");
     });
+
     it("dog.bark should return 'Woof woof'", function() {
       expect(dog.bark()).toBe("Woof woof");
     });
@@ -113,6 +125,7 @@ describe("js-day1-basic-assessment", function() {
     it("ruff should exist", function() {
       expect(ruff).toBeDefined();
     });
+
     it("ruff should be 'Woof woof'", function() {
       expect(ruff).toBe("Woof woof");
     });
@@ -123,9 +136,11 @@ describe("js-day1-basic-assessment", function() {
       expect(looper).toBeDefined();
       expect(typeof looper).toBe("function");
     });
+
     it("looper should return 1109 when passed [1, 2, 3, 5, 100, 1000, 6]", function() {
       expect(looper([1, 2, 3, 5, 100, 1000, 6])).toEqual(1109);
     });
+
     it("looper should loop through the array and sum all odd numbers & number greater than 100 (inclusive)", function() {
       myArr = [3, 5, 250, 4, 2];
       let correct = looper(myArr) === 258;
@@ -137,6 +152,7 @@ describe("js-day1-basic-assessment", function() {
     it("add should exist", function() {
       expect(add).toBeDefined();
     });
+
     it("add should return the sum of its two parameters", function() {
       expect(add(1, 2) === 3 && add("hello", "world") === "helloworld").toBe(
         true
@@ -152,12 +168,15 @@ describe("js-day1-basic-assessment", function() {
     it("invoker should exist", function() {
       expect(invoker).toBeDefined();
     });
+
     it("invoker should return 'I am a callback function' when passed sampleCallbackOne", function() {
       expect(invoker(sampleCallbackOne)).toBe("I am a callback function");
     });
+
     it("invoker should return 'I am also a callback function' when passed sampleCallbackTwo", function() {
       expect(invoker(sampleCallbackTwo)).toBe("I am also a callback function");
     });
+
     it("invoker should return 'ducks are cute' when passed the anonymous function () => 'ducks are cute'", function() {
       expect(invoker(() => "ducks are cute")).toBe("ducks are cute");
     });
@@ -172,6 +191,7 @@ describe("js-day1-basic-assessment", function() {
         globalScope.includes("realDuck");
       expect(containsGlobals && !containsOutOfScopeVars).toBe(true);
     });
+
     it("bathroomScope should contain only variables accessible in the scope of the function bathroom", function() {
       let containsGlobals = arrayIncludes(bathroomScope, [
         "duck",
@@ -182,6 +202,7 @@ describe("js-day1-basic-assessment", function() {
         bathroomScope.includes("realDuck");
       expect(containsGlobals && !containsOutOfScopeVars).toBe(true);
     });
+
     it("bathtubScope should contain only variables accessible in the scope of the function bathtub", function() {
       let containsGlobals = arrayIncludes(bathtubScope, [
         "duck",
@@ -191,6 +212,7 @@ describe("js-day1-basic-assessment", function() {
       let containsOutOfScopeVars = bathtubScope.includes("realDuck");
       expect(containsGlobals && !containsOutOfScopeVars).toBe(true);
     });
+
     it("pondScope should contain only variables accessible in the scope of the function pond", function() {
       let containsGlobals = arrayIncludes(pondScope, ["duck", "realDuck"]);
       let containsOutOfScopeVars =
@@ -203,9 +225,11 @@ describe("js-day1-basic-assessment", function() {
     it("outerFn should exist", function() {
       expect(outerFn).toBeDefined();
     });
+
     it("outerFn should return a function", function() {
       expect(typeof outerFn()).toBe("function");
     });
+
     it("outerFn should return a function that returns a string", function() {
       expect(typeof outerFn()()).toBe("string");
     });
@@ -213,9 +237,11 @@ describe("js-day1-basic-assessment", function() {
     it("innerFn should exist", function() {
       expect(outerFn).toBeDefined();
     });
+
     it("innerFn should be a function", function() {
       expect(typeof innerFn).toBe("function");
     });
+
     it("innerFn should return a string", function() {
       expect(typeof innerFn()).toBe("string");
     });
@@ -223,6 +249,7 @@ describe("js-day1-basic-assessment", function() {
     it("finalResult should exist", function() {
       expect(finalResult).toBeDefined();
     });
+
     it("innerFn should return a string", function() {
       expect(typeof finalResult).toBe("string");
     });
