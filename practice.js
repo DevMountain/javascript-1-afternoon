93dd81cb1b31eccdc4c05e22b08bf1b6059c1914
Umar <55187735+umarfaruq43@users.noreply.github.com>
2020-01-23 10:27:37 +0100

    working on basics of javascript
    i answered the java Script questions

--- practice.js
+++ practice.js
@@ -3,31 +3,32 @@
 //Create a variable called myName that is a string data type
 
 //Code here
-
+var myName = "faruq";
 //////////////////PROBLEM 2////////////////////
 
 //Create a variable called myAge that is a number data type
 
 //Code here
-
+var myAge = 12;
 //////////////////PROBLEM 3////////////////////
 
 //Create a variable called lovesCode that is a boolean data type
 
 //Code here
-
+var lovesCode = console.log(myAge);
 //////////////////PROBLEM 4////////////////////
 
 //Create a variable called greatestFear that is undefined because we fear nothing
 
 //Code here
+var greatestFear = "undefined";
 
 //////////////////PROBLEM 5////////////////////
 
 //Create a variable called devMountainGoal that is null because we are just starting out
 
 //Code here
-
+var devMountainGoal = null;
 //////////////////PROBLEM 6////////////////////
 
 //Create a function declaration called greeting that
@@ -36,6 +37,11 @@
 //plus the value of the name parameter.
 
 //Code here
+function greeting () {
+  prompt("what is your name?");
+   alert("Hello " + name);
+}
+  
 
 //////////////////PROBLEM 7////////////////////
 
@@ -43,6 +49,10 @@
 //Give it the same functionality as the function greeting in Problem 6.
 
 //Code Here
+function newGreeting () {
+  prompt("what is your name?");
+   alert("Hello " + name);
+}
 
 //////////////////PROBLEM 8////////////////////
 
@@ -50,6 +60,7 @@
 //"apples", "milk", "eggs", "bread"
 
 //Code Here
+var groceries = ["apples", "milk", "eggs", "bread"];
 
 //////////////////PROBLEM 9////////////////////
 
@@ -59,12 +70,18 @@
 //and goodBoy (a boolean).
 
 //Code Here
-
+ var dog = {
+   name = 'jim',
+   color = 'red',
+   age = 12,
+   goodboy = console.log(goodboy);
+ }
 //...access the dog's name from the object and assign it to a
 //variable called devMountainClassPet.
-
+ 
 //Code Here
 
+var  devMountainClassPet = dog.name;
 //////////////////PROBLEM 10////////////////////
 
 //Write a function called nameCheck that takes in a name parameter.
@@ -73,9 +90,15 @@
 //If the name parameter is Bryan, return 'Hey Bryan!'
 // If the name parameter is anything else, return 'Cool name, NAMEPARAM'
 // with NAMEPARAM being the name parameter being passed in (not literally NAMEPARAM)
-
+********************************************************************************************************************************
 // Code here
-
+function nameCheck () {
+  var name = prompt("what is your name?");
+  if (name === "steven") {
+    return ("what is up steven");
+  } 
+  
+    
 //////////////////PROBLEM 11////////////////////
 
 // Create a function called add that takes in two parameters
@@ -83,11 +106,14 @@
 // The add function should return the two parameters added together
 
 //Code Here
-
+function add (a, b) {
+  return (a + b);
+}
 //Now invoke add, passing in the numbers 3 and 4
 //storing the result in the variable mathSum.
 
 //Code Here
+  var mathSum = add(3, 4);
 
 //////////////////PROBLEM 12////////////////////
 
@@ -97,9 +123,20 @@
 // If the passed in color equals 'green', return 'green is a solid favorite color'
 // If the passed in color equals 'black', return 'so trendy'
 // Otherwise, you should return the string 'you need to evaluate your favorite color choice'
-
+***********************************************************************************************************************************
 // Code here
-
+function faveColorFinder () {
+  var color = prompt("what is your favorite color");
+  if (color == "red") { 
+    return ("red is a solid favorite color");
+  }
+  if (color == "green") { 
+    return ("green is a solid favorite color");
+  }
+  if (color == "black") { 
+    return ("so trendy");
+  }
+ 
 //////////////////PROBLEM 13////////////////////
 
 let duck = "cute";
@@ -120,6 +157,7 @@ function pond() {
 //Given the functions and variables above, edit the arrays
 //below to contain only the appropriate variable names
 //as strings.
+  
 
 //This array should contain the variable names (as strings) accessible in the global scope.
 let globalScope = ["duck", "sailorDuck", "rubberDuck", "realDuck"];
